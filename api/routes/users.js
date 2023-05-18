5a814676e674343156e483ecf52d696ee174cb96
Thales Spanhol <thales_spanhol@hotmail.com>
2023-05-17 21:24:44 -0300

    Fix: refatoração api

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,14 +1,19 @@
 import express from "express";
-import { getUsers, addUser, updateUser, deleteUser } from '../controllers/user.js'
+import {
+  getUsers,
+  addUser,
+  updateUser,
+  deleteUser,
+} from "../controllers/user.js";
 
 const router = express.Router();
 
-router.get('/', getUsers);
+router.get("/", getUsers);
 
-router.post('/', addUser);
+router.post("/", addUser);
 
-router.put('/:id', updateUser);
+router.put("/:id", updateUser);
 
-router.delete('/:id', deleteUser);
+router.delete("/:id", deleteUser);
 
 export default router;
