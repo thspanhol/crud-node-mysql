a8d9ae210b9624329cedb13eb12a32e6df503d55
Thales Spanhol <thales_spanhol@hotmail.com>
2023-05-18 17:55:30 -0300

    Fix: refatoração frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,20 @@
-import GlobalStyle from './styles/global';
-import styled from 'styled-components';
-import Form from './components/Form.js';
-import Grid from './components/Grid';
-import { useEffect, useState } from 'react';
-import { toast, ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import axios from 'axios';
+import GlobalStyle from "./styles/global";
+import styled from "styled-components";
+import Form from "./components/Form.js";
+import Grid from "./components/Grid";
+import { useEffect, useState } from "react";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
 
 const Container = styled.div`
-width: 100%;
-max-width: 800px;
-margin-top: 20px;
-display: flex;
-flex-direction: column;
-align-items: center;
-gap: 10px;
+  width: 100%;
+  max-width: 800px;
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
 `;
 
 const Title = styled.h2``;
@@ -25,7 +25,7 @@ function App() {
 
   const getUsers = async () => {
     try {
-      const res = await axios.get('http://localhost:8800');
+      const res = await axios.get("http://localhost:8800");
       setUsers(res.data.sort((a, b) => (a.nome > b.nome ? 1 : -1)));
     } catch (error) {
       toast.error(error);
@@ -38,13 +38,13 @@ function App() {
 
   return (
     <>
-    <Container>
-      <Title>USUÁRIOS</Title>
-      <Form onEdit={onEdit} setOnEdit={setOnEdit} getUsers={getUsers} />
-      <Grid users={users} setUsers={setUsers} setOnEdit={setOnEdit} />
-    </Container>
-    <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT} />
-    <GlobalStyle />
+      <Container>
+        <Title>USUÁRIOS</Title>
+        <Form onEdit={onEdit} setOnEdit={setOnEdit} getUsers={getUsers} />
+        <Grid users={users} setUsers={setUsers} setOnEdit={setOnEdit} />
+      </Container>
+      <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT} />
+      <GlobalStyle />
     </>
   );
 }
